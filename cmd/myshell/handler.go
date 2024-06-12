a79c5348a00f65d9edb2a07a832b202166045a44
Te4nick <ivan.ivantz@gmail.com>
2024-06-12 23:39:32 +0300

    pass 6th stage

---
+++ cmd/myshell/handler.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type CMD func(writer *bufio.Writer, reader *bufio.Reader, args []string) error
+
+type Handler struct {
+	comMap map[string]CMD
+}
+
+func NewHandler() *Handler {
+	h := &Handler{
+		comMap: make(map[string]CMD),
+	}
+
+	h.Register("exit", h.builtinExit)
+	h.Register("echo", h.builtinEcho)
+
+	return h
+}
+
+func (h *Handler) Register(name string, fn CMD) {
+	h.comMap[name] = fn
+}
+
+func (h *Handler) REPL(writer *bufio.Writer, reader *bufio.Reader) error {
+	for {
+		WriteString(writer, "$ ")
+		command := ReadString(reader)
+		command = strings.TrimSuffix(command, "\n")
+		args := strings.Split(command, " ")
+
+		cmd, ok := h.comMap[args[0]]
+		if !ok {
+			WriteStringln(writer, fmt.Sprintf("%s: command not found", args[0]))
+			continue
+		}
+
+		err := cmd(writer, reader, args)
+		if err != nil {
+			WriteStringln(writer, fmt.Sprintf("%s: %s", args[0], err.Error()))
+		}
+	}
+}
+
+func (h *Handler) builtinExit(_ *bufio.Writer, _ *bufio.Reader, args []string) error {
+	if len(args) == 0 {
+		return errors.New("no exit code provided")
+	}
+
+	exitCode, err := strconv.Atoi(args[1])
+	if err != nil {
+		return errors.New("exit code must be an integer")
+
+	}
+
+	os.Exit(exitCode)
+	return nil
+}
+
+func (h *Handler) builtinEcho(writer *bufio.Writer, _ *bufio.Reader, args []string) error {
+	WriteStringln(writer, strings.Join(args[1:], " "))
+	return nil
+}
--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func WriteString(w *bufio.Writer, s string) {
@@ -35,21 +32,10 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 
-	for {
-		WriteString(writer, "$ ")
-		command := ReadString(reader)
-		command = strings.TrimSuffix(command, "\n")
-		args := strings.Split(command, " ")
-		switch args[0] {
-		case "exit":
-			exitCode, err := strconv.Atoi(args[1])
-			if err != nil {
-				WriteStringln(writer, "error: argument must be an integer")
-				break
-			}
-			os.Exit(exitCode)
-		default:
-			WriteStringln(writer, fmt.Sprintf("%s: command not found", args[0]))
-		}
+	h := NewHandler()
+
+	err := h.REPL(writer, reader)
+	if err != nil {
+		panic(err)
 	}
 }
